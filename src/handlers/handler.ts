469ee8ea456c76ef0da42efb71b4e2005afc6b0c
Mebyus <31649307+Mebyus@users.noreply.github.com>
2021-02-27 19:02:29 +0400

    Slight refactoring for handling ws messages

---
+++ src/handlers/handler.ts
@@ -0,0 +1,34 @@
+import * as auth from '../auth/auth';
+import * as task from '../database/task';
+import * as dbtype from '../types/server/database';
+import * as request from '../types/shared/request';
+
+export function register(req: request.Register): Promise<any> {
+    return auth.addUser(req.payload);
+}
+
+export function verify(req: request.Login): Promise<any> {
+    return auth.verify(req.payload.login, req.payload.password);
+}
+
+export function taskList(req: request.TaskList): Promise<any> {
+    return task.getByUserId(1); // need uder id here
+}
+
+export function newTask(req: request.NewTask): Promise<any> {
+    return task.insert(req.payload, 1); // need uder id here
+}
+
+export function archiveTask(req: request.ArchiveTask): Promise<any> {
+    return task.archive(req.payload.id);
+}
+
+export function updateTask(req: request.UpdateTask): Promise<any> {
+    return task.update(req.payload as dbtype.Task);
+}
+
+export function unknown(req: request.Client): Promise<any> {
+    return new Promise((resolve, reject) => {
+        reject('unknown request route: ' + req.route);
+    });
+}
--- src/ws-router.ts
+++ src/ws-router.ts
@@ -1,11 +1,8 @@
 import WebSocket = require('ws');
-import * as auth from './auth/auth';
-import * as task from './database/task';
-import * as dbtype from './types/server/database';
+import * as handler from './handlers/handler';
 import * as error from './types/shared/error';
 import * as message from './types/shared/message';
 import * as request from './types/shared/request';
-// import query = require('./database/query');
 
 export class MessageRouter {
     private readonly ws: WebSocket;
@@ -27,44 +24,33 @@ export class MessageRouter {
             return;
         }
 
-        let result: Promise<any>;
-        switch (req.route) {
-            case 'register':
-                const regReq: request.Register = req as request.Register;
-                result = auth.addUser(regReq.payload);
-                break;
-            case 'session-info':
-            case 'login':
-                const loginReq: request.Login = req as request.Login;
-                result = auth.verify(loginReq.payload.login, loginReq.payload.password);
-                break;
-            case 'task-list':
-                result = task.getByUserId(1); // need uder id here
-                break;
-            case 'new-task':
-                const newReq: request.NewTask = req as request.NewTask;
-                result = task.insert(newReq.payload, 1); // need uder id here
-                break;
-            case 'archive-task':
-                const archiveReq: request.ArchiveTask = req as request.ArchiveTask;
-                result = task.archive(archiveReq.payload.id);
-                break;
-            case 'update-task':
-                const updateReq: request.UpdateTask = req as request.UpdateTask;
-                result = task.update(updateReq.payload as dbtype.Task);
-                break;
-            default:
-                console.log('Unknown request route:', req.route);
-                return;
-        }
-
-        result
+        this.handleRequest(req)
             .then((payload: any): void => {
                 this.reply(req.route, payload);
             })
             .catch((reason: any): void => {
                 this.errorReply(reason);
             });
+    }
+
+    private handleRequest(req: request.Client): Promise<any> {
+        switch (req.route) {
+            case 'register':
+                return handler.register(req as request.Register);
+            case 'session-info':
+            case 'login':
+                return handler.verify(req as request.Login);
+            case 'task-list':
+                return handler.taskList(req as request.TaskList);
+            case 'new-task':
+                return handler.newTask(req as request.NewTask);
+            case 'archive-task':
+                return handler.archiveTask(req as request.ArchiveTask);
+            case 'update-task':
+                return handler.updateTask(req as request.UpdateTask);
+            default:
+                return handler.unknown(req);
+        }
     }
 
     private errorReply(e: any): void {
